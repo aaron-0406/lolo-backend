4158229bc5b1f95a2dec94d40bb1d5e44c60ea4b
Aaron Paredes Cabrera <iron0406@outlook.com>
2022-12-04 14:48:09 -0500

    chore: update URI for mysql

--- src/libs/sequelize.ts
+++ src/libs/sequelize.ts
@@ -4,7 +4,7 @@ import config from "../config/config";
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
 
-const URI = "";
+const URI = `mysql://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
 const sequelize = new Sequelize(URI, {
   dialect: "mysql",
