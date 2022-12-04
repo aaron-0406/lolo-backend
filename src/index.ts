2d2f46f2b41989166bf402a3b9dd66ad1e60c687
Aaron Paredes Cabrera <iron0406@outlook.com>
2022-12-04 01:31:29 -0500

    chore: add project settings

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import express from "express";
+import cors, { CorsOptions } from "cors";
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+
+//CORS
+const whitelist = ["http://localhost:8080", "https://myapp.co"];
+const options: CorsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin ?? "") || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("no permitido"));
+    }
+  },
+};
+app.use(cors(options));
+
+app.get("/", (req, res) => {
+  res.send("Hola mi server en express");
+});
+
+app.get("/nueva-ruta", (req, res) => {
+  res.send("Hola, soy una nueva ruta");
+});
+
+app.listen(port, () => {
+  console.log("My port: " + port);
+});
