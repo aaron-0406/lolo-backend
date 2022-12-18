da1bb5e7701f0190492bd790f2f9f51404dd6587
Aaron Paredes Cabrera <iron0406@outlook.com>
2022-12-18 17:38:54 -0500

    chore: add funcionario model, schema and type component

--- src/app/boss/schemas/funcionario.schema.ts
+++ src/app/boss/schemas/funcionario.schema.ts
@@ -4,11 +4,13 @@ import { FuncionarioType } from "../types/funcionario.type";
 const id = Joi.number();
 const name = Joi.string().min(1).max(150);
 const createAt = Joi.date();
+const bankID = Joi.number();
 
 const createFuncionarioSchema = Joi.object<FuncionarioType, true>({
   id: id.required(),
   name: name.required(),
   createdAt: createAt.optional(),
+  bankID: bankID.required(),
 });
 
 const getFuncionarioSchema = Joi.object<{ id: number }, true>({
---
+++ src/db/models/funcionario.model.ts
@@ -0,0 +1,60 @@
+import {
+  Model,
+  DataTypes,
+  Sequelize,
+  ModelAttributes,
+  ModelCtor,
+} from "sequelize";
+import { FuncionarioType } from "../../app/boss/types/funcionario.type";
+import bankModel from "./bank.model";
+
+const FUNCIONARIO_TABLE = "FUNCIONARIO";
+
+const FuncionarioSchema: ModelAttributes<Funcionario, FuncionarioType> = {
+  id: {
+    primaryKey: true,
+    allowNull: false,
+    autoIncrement: true,
+    field: "id_funcionario",
+    type: DataTypes.INTEGER,
+  },
+  name: {
+    allowNull: false,
+    field: "name",
+    type: DataTypes.STRING(150),
+  },
+  createdAt: {
+    allowNull: false,
+    field: "created_at",
+    defaultValue: DataTypes.NOW,
+    type: DataTypes.DATE,
+  },
+  bankID: {
+    allowNull: false,
+    field: "bank_id_bank",
+    type: DataTypes.INTEGER,
+    references: {
+      model: bankModel.BANK_TABLE,
+      key: "id_BANK",
+    },
+    onUpdate: "CASCADE",
+    onDelete: "NO ACTION",
+  },
+};
+
+class Funcionario extends Model {
+  static associate(models: { [key: string]: ModelCtor<Model> }) {
+    this.belongsTo(models.FUNCIONARIO, { as: "bank" });
+  }
+
+  static config(sequelize: Sequelize) {
+    return {
+      sequelize,
+      tableName: FUNCIONARIO_TABLE,
+      modelName: FUNCIONARIO_TABLE,
+      timestamps: false,
+    };
+  }
+}
+
+export default { FUNCIONARIO_TABLE, FuncionarioSchema, Funcionario };
