0f87166fde27e5bdecc3a45c9ce34f08872c7d50
Victor <victorhv2729@gmail.com>
2023-01-09 01:55:36 -0500

    feat:

--- src/middlewares/error.handler.ts
+++ src/middlewares/error.handler.ts
@@ -31,7 +31,7 @@ const boomErrorHandler = (
 ) => {
   if (err.isBoom) {
     const { output } = err;
-    res.status(output.statusCode).json(output.payload);
+    return res.status(output.statusCode).json(output.payload);
   }
   next(err);
 };
