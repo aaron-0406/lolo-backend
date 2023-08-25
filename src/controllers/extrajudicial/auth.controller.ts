f1389e3ade0b42f749f02b31f5d6fbfbce3d274f
Victor Hernandez <victor-2027@hotmail.com>
2023-08-25 15:53:34 -0500

    feat: :sparkles: modal credentials created, modal update password

--- src/controllers/extrajudicial/auth.controller.ts
+++ src/controllers/extrajudicial/auth.controller.ts
@@ -3,8 +3,12 @@ import passport from "passport";
 import { CustomerUserType } from "../../app/dash/types/customer-user.type";
 import { signToken } from "../../libs/jwt";
 import AuthService from "../../app/extrajudicial/services/auth.service";
+import PermissionService from "../../app/dash/services/permission.service";
+import CustomerUserService from "../../app/dash/services/customer-user.service";
 
 const serviceAuth = new AuthService();
+const servicePermission = new PermissionService();
+const serviceCustomerUser = new CustomerUserService();
 
 export const loginController = async (
   req: Request,
@@ -36,6 +40,29 @@ export const changePasswordController = async (
       Number(req.user?.id)
     );
     return res.json({ success: "Contraseña modificada" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const changeCredentialsController = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    await serviceAuth.changeCredentials(req.body, Number(req.user?.id));
+    const user = await serviceCustomerUser.findOne(String(req.user?.id));
+    const permissions = await servicePermission.findAllByRoleId(
+      user.dataValues.roleId
+    );
+    const codes = permissions.map((permissions) => permissions.code);
+    const customerUser = { ...user.dataValues, permissions: codes };
+    const token = signToken(customerUser, `${process.env.JWT_SECRET}`);
+    return res.json({
+      user: customerUser,
+      token,
+    });
   } catch (error) {
     next(error);
   }
