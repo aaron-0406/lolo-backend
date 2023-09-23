52ad07b91d15a4b257d11c188fa59c05ff3114df
Bren pc <brayanp.cabrera@gmail.com>
2023-09-23 15:42:44 -0500

    token expire in four hours

--- src/libs/jwt.ts
+++ src/libs/jwt.ts
@@ -1,9 +1,9 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 // Sign token function
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const signToken = (payload: any, secret: string) => {
-  return jwt.sign(payload, secret);
+  return jwt.sign(payload, secret, { expiresIn: 14400 });
 };
 
 // Verify token
