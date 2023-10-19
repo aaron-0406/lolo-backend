898c541f57951a3c20d4504f4141ec2d8bc9c1ff
aaron-0406 <iron0406@outlook.com>
2023-10-18 20:38:45 -0500

    chore: last changes

---
+++ src/db/migrations/20231018215626-add_new_routes_for_judicial.ts
@@ -0,0 +1,25 @@
+import { QueryInterface } from "sequelize";
+import permissionModel from "../models/permission.model";
+
+const { PERMISSION_TABLE } = permissionModel;
+
+export async function up(queryInterface: QueryInterface) {
+  return queryInterface.bulkInsert(PERMISSION_TABLE, [
+    {
+      name: "EXPEDIENTES",
+      code: "P13",
+      icon: "ri-bank-fill",
+      link: "judicial/:urlIdentifier/expedientes",
+    },
+    {
+      name: "DETALLES DEL EXPEDIENTE",
+      code: "P13-01",
+      icon: "ri-bank-fill",
+      link: "judicial/:urlIdentifier/expedientes/:code",
+    },
+  ]);
+}
+
+export async function down(queryInterface: QueryInterface) {
+  return queryInterface.bulkDelete(PERMISSION_TABLE, {});
+}
