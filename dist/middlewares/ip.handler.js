a504f3c4a9eb97e0f761071c7f39b85028c5bda4
aaron-0406 <iron0406@outlook.com>
2023-11-19 00:02:44 -0500

    chore: getting ipv4

--- dist/middlewares/ip.handler.js
+++ dist/middlewares/ip.handler.js
@@ -1,8 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const getClientIp = (req) => {
-    const clientIP = (req.headers["x-forwarded-for"] || req.ip || "");
-    return clientIP.split(",")[0].trim();
+    const forwardedFor = req.headers["x-forwarded-for"];
+    const clientIP = forwardedFor
+        ? forwardedFor.split(",")[0].trim()
+        : req.ip || "";
+    const ipv4Address = extractIPv4(clientIP);
+    return ipv4Address || "";
+};
+const extractIPv4 = (ip) => {
+    const ipv4Regex = /(\d+\.\d+\.\d+\.\d+)/;
+    const match = ip.match(ipv4Regex);
+    return match ? match[0] : null;
 };
 const ipHandler = (req, res, next) => {
     req.clientIp = getClientIp(req);
--- src/middlewares/ip.handler.ts
+++ src/middlewares/ip.handler.ts
@@ -1,8 +1,20 @@
 import { Request, Response, NextFunction } from "express";
 
 const getClientIp = (req: Request) => {
-  const clientIP = (req.headers["x-forwarded-for"] || req.ip || "") as string;
-  return clientIP.split(",")[0].trim();
+  const forwardedFor = req.headers["x-forwarded-for"] as string;
+  const clientIP = forwardedFor
+    ? forwardedFor.split(",")[0].trim()
+    : req.ip || "";
+
+  const ipv4Address = extractIPv4(clientIP);
+
+  return ipv4Address || "";
+};
+
+const extractIPv4 = (ip: string) => {
+  const ipv4Regex = /(\d+\.\d+\.\d+\.\d+)/;
+  const match = ip.match(ipv4Regex);
+  return match ? match[0] : null;
 };
 
 const ipHandler = (req: Request, res: Response, next: NextFunction) => {
