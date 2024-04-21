f0e1ae3bef78ccb0dd2b7c0b94b4dbfd2d77a08f
Aaron Paredes Cabrera <iron0406@outlook.com>
2024-04-21 12:44:17 -0500

    chore: rename chb to customer

--- src/routes/dash/city.routes.ts
+++ src/routes/dash/city.routes.ts
@@ -15,7 +15,7 @@ const { getCitySchema, createCitySchema, updateCitySchema, getCitiesSchema } =
 const router = express.Router();
 
 router.get(
-  "/chb/:customerId",
+  "/customer/:customerId",
   JWTAuth,
   validatorHandler(getCitiesSchema, "params"),
   getAllCityController
